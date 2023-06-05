4153ce8941500f18587f5c2975e899fe728f56bb
fabiodigiacomopepe <fabiodgp20@gmail.com>
2023-06-05 14:58:36 +0200

    first commit

---
+++ src/App.vue
@@ -0,0 +1,19 @@
+<script>
+import HelloWorld from './components/HelloWorld.vue'
+
+export default {
+  components: {
+    HelloWorld,
+  }
+}
+</script>
+
+<template>
+  <HelloWorld />
+</template>
+
+<style lang="scss">
+@use './styles/general.scss' as *;
+@use './styles/partials/variables.scss' as *;
+@use './styles/partials/mixins.scss' as *;
+</style>
---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,12 @@
+<script>
+export default {
+  name: "HelloWorld",
+}
+</script>
+
+<template></template>
+
+<style scoped lang="scss">
+@use '../styles/partials/variables.scss' as *;
+@use '../styles/partials/mixins.scss' as *;
+</style>
